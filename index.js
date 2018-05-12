afdab950f43c1df2797911809ac3767004a8bf97
karissa <krmckelv@gmail.com>
2018-05-12 17:58:14 -0500

    add docuemntation

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ var swarm = require('./swarm')
 
 module.exports =  Mesh
 
+/**
+ * Create a new Chatmesh. This is the object handling all
+ * local nickname -> mesh interactions for a single user.
+ * @constructor
+ * @param {string|function} storage - A hyperdb compatible storage function, or a string representing the local data path.
+ * @param {string} href - The dat link
+ * @param {Object} opts - Options include: username
+ */
 function Mesh (storage, href, opts) {
   if (!(this instanceof Mesh)) return new Mesh(storage, href, opts)
   if (!opts) opts = {}
@@ -39,6 +47,12 @@ function Mesh (storage, href, opts) {
 
 inherits(Mesh, events.EventEmitter)
 
+/**
+ * When a connection is made. Auto-authorizes new peers to
+ * write to the local database. Maintains the local view
+ * of visible users.
+ * @param {Object} peer - The discovery-swarm peer emitted from the 'connection' or 'disconnection' event
+ */
 Mesh.prototype.onconnection = function (peer) {
   var self = this
   if (!peer.remoteUserData) return
@@ -65,6 +79,12 @@ Mesh.prototype.onconnection = function (peer) {
   }
 }
 
+/**
+ * Create a message.
+ * @param {String} message - The message to write.
+ * @param {Object} opts - Options: date, username
+ * @param {function} done - When message has been successfully added.
+ */
 Mesh.prototype.message = function (message, opts, done) {
   if (typeof opts === 'function') return this.message(message, null, opts)
   if (!opts) opts = {}
